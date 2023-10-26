7175ad751f560f592ea066927bd396eba5b6bbe4
Brayan17king <104658332+Brayan17king@users.noreply.github.com>
2023-10-26 18:56:30 -0500

    feat: :arrow_up: New Dependency

--- API/Program.cs
+++ API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.ConfigureCors();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddApplicationServices();
 
 builder.Services.AddDbContext<IncidenciaContext>(optionsBuilder =>
 {
